4ccc0ff74217e28eed930b026a906bdcf31fb4ee
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2021-05-16 13:12:38 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ sudo docker build -t kafka KafkaDocker/Dockerfile
 
 sudo docker build -t deepstream DeepstreamDocker/Dockerfile
 
-sudo docker run -it --name=kafka
+sudo docker run -d --name=kafka --network=host kafka
 
-sudo docker run -it --name=deepstream
+sudo docker run -it --name=deepstream --gpus all -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -w /opt/nvidia/deepstream/deepstream-5.1 deepstream
